1a97d72ba8df01a8cb113e68e0efc71bdb365c28
jsl15 <jessicasliang@gmail.com>
2015-02-21 01:33:13 -0800

    Added the login view

---
+++ Popcorn/Popcorn/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Popcorn
+//
+//  Created by Jessica Liang on 2/21/15.
+//  Copyright (c) 2015 Nate Parrott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
